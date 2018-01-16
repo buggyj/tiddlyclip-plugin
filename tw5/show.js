ff3283bd95265409ae8aa80e3c7b38ec18345505
buggyj <buggyjef@gmail.com>
2018-01-16 08:59:17 +0100

    add extra paras to show func for  #42

--- tw5/show.js
+++ tw5/show.js
@@ -1,8 +1,10 @@
- function show(action,text) {
+ function show(action,text,aux,extra) {
 	var message = document.createElement("div") ,messageBox = document.getElementById("tiddlyclip-message-box");
 	if(messageBox) {
 		message.setAttribute("data-action",action);
-		message.setAttribute("data-text",text);
+		message.setAttribute("data-text",text||"");
+		message.setAttribute("data-aux",aux||"");
+		message.setAttribute("data-extra",extra||"");
 		messageBox.appendChild(message);
 		
 		// Create and dispatch the custom event to the extension
@@ -19,11 +21,11 @@
 
 
 exports.name ="show";
-exports.run  = function(action,text) {
+exports.run  = function(action,text,aux,extra) {
 
 if (!action) return "nothing to do!";
 
-return  show(action,text);
+return  show(action,text,aux,extra);
 
 
 };
