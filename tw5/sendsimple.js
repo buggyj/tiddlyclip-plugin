32913ada9955f257f3d43248b44332178148ded5
buggyj <buggyjef@gmail.com>
2023-06-19 10:52:49 +0200

    simple send for sidenote - ingores tc dock status

---
+++ tw5/sendsimple.js
@@ -0,0 +1,36 @@
+
+ function send(action,text,aux,extra) {
+	var message = document.createElement("div") ,messageBox = document.getElementById("tiddlyclip-message-box");
+	if(messageBox) {
+		message.setAttribute("data-action",action);
+		message.setAttribute("data-text",text||"");
+		message.setAttribute("data-aux",aux||"");
+		message.setAttribute("data-extra",extra||"");
+		messageBox.appendChild(message);
+		
+		// Create and dispatch the custom event to the extension
+		var event = document.createEvent("Events");
+		event.initEvent("tc-send-event",true,false);
+		message.dispatchEvent(event);
+     return "sent";
+	} else {
+		return "error no  extension found";
+	}
+};
+
+
+
+
+exports.name ="sendsimple";
+exports.run  = function(action,text,aux,extra) {
+
+	if (!action) return "nothing to do!";
+	//if docking, set the config to this table for mapping returned clips
+	if (action === "dock") {
+		var ob = JSON.parse(text),opts = JSON.parse(aux);
+		tiddlyclip.modules.tPaste.setconfig(ob.text,ob.title);
+		tiddlyclip.modules.tPaste.setopts(opts.text,opts.title);
+		extra = extra || document.title;
+	}
+	return  send(action,text,aux,extra);
+};
