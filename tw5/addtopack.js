326c2ebdd34382fc327817b79c4f93f76ca61471
buggyj <buggyjef@gmail.com>
2023-07-10 17:32:12 +0200

    rename of function and tiedup

--- tw5/addtopack.js
+++ tw5/addtopack.js
@@ -1,11 +1,12 @@
 //copy all variables with a given prefix into an object.
-exports.name ="addToPack";
+exports.name ="addToObject";
 exports.run  = function(pack,value,name,type) {
 //---------------define function below --------------------
-var val, inval = this._g(value), json;
+var val, inval = value, json;
+if (type == "ref" || type == 'reflist') inval = this._g(value);
 if (!inval) return pack;
 json = JSON.parse (pack);
-if (type == 'list') json[name] = this._parseStringArray(inval);
+if (type == 'list' || type == 'reflist') json[name] = this._parseStringArray(inval);
 else json[name] = inval;
 
 return JSON.stringify(json);
