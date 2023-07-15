12583b8e0f1d8827f703065462083a858889724d
buggyj <buggyjef@gmail.com>
2023-07-15 15:59:49 +0200

    rename pagedata vars from sidenote to match tc

--- tw5/addtopack.js
+++ tw5/addtopack.js
@@ -4,7 +4,7 @@ exports.run  = function(pack,value,name,type) {
 //---------------define function below --------------------
 var val, inval = value, json;
 if (type == "ref" || type == 'reflist') inval = this._g(value);
-if (!inval) return pack;
+if (typeof inval !== "string") return pack;
 json = JSON.parse (pack);
 if (type == 'list' || type == 'reflist') json[name] = this._parseStringArray(inval);
 else json[name] = inval;
---
+++ tw5/callback.js
@@ -0,0 +1,5 @@
+exports.name ="callback";
+exports.run  = function(text,aux,extra) {
+	  var result = this.sendsimple(this._g("@__callback__"),text,aux,extra);
+
+};
